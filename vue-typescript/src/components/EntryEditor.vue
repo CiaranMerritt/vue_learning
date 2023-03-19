03f3166ca8c22fc2de28a7c9ad9a08cec480be7b
CiaranMerritt <ciaran.merritt@colliewobble.com>
2023-03-19 17:17:05 +0000

    feat: Adding an emit for the form submit

--- vue-typescript/src/components/EntryEditor.vue
+++ vue-typescript/src/components/EntryEditor.vue
@@ -1,13 +1,52 @@
 <script lang="ts" setup>
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import { computed, ref } from "vue";
+import type Emjoi from "@/types/Emjoi";
+
+//#region Variables
+const text = ref<string>("");
+const emoji = ref<Emjoi | null>(null);
+const maxCharacters = ref<number>(280);
+
+//#endregion
+
+//#region Computed
+const charCount = computed<number>(() => text.value.length);
+//#endregion
+
+//#region Emits
+/*
+defineEmits is a macro that when compiles changes to browser ready code of
+<script>
+export default {
+  emits: ["create"]
+}
+<script>
+*/
+defineEmits(["create"]);
+
+//#endregion
+
+//#region Methods
+const handleTextInput = (e: Event) => {
+  const textArea = e.target as HTMLTextAreaElement;
+  console.log(textArea.value);
+};
+
+//#endregion
 </script>
 <template>
-  <form class="entry-form" @submit.prevent>
-    <textarea placeholder="New Journal Entry for danielkelly_io"></textarea>
-    <EmojiField />
+  <form class="entry-form" @submit.prevent="$emit('create', { text, emoji })">
+    <textarea
+      v-model="text"
+      :maxlength="maxCharacters"
+      placeholder="New Journal Entry for danielkelly_io"
+      @keyup="handleTextInput"
+    ></textarea>
+    <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <span>{{ charCount }} / {{ maxCharacters }}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
