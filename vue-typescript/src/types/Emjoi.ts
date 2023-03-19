b342845b568af5fa1f17c1de26183444b8df6413
CiaranMerritt <ciaran.merritt@colliewobble.com>
2023-03-19 17:17:23 +0000

    feat: Adding the type for emoji

---
+++ vue-typescript/src/types/Emjoi.ts
@@ -0,0 +1,17 @@
+type Emjoi =
+  | "confused"
+  | "cool"
+  | "crying"
+  | "grinning-squinting"
+  | "happy"
+  | "heart-eyes"
+  | "laughing"
+  | "sad"
+  | "shocked"
+  | "smiling"
+  | "smiling-with-eyes"
+  | "surprised"
+  | "wink"
+  | "worried";
+
+export default Emjoi;
