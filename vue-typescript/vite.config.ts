26c26c0b568f0a3b26ded83aaf3d3368a009a66b
CiaranMerritt <ciaran.merritt@colliewobble.com>
2023-03-20 21:00:03 +0000

    chore: Changing the upload path and removing the dist folder

--- vue-typescript/vite.config.ts
+++ vue-typescript/vite.config.ts
@@ -6,11 +6,14 @@ import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue_learning/vue-typescript/",
+  base: "/vue_learning/vue-ts/",
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  build: {
+    outDir: "./dist/vue-ts",
   },
 });
