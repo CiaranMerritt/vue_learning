6b02259cbce52927f0191744d22555059207a236
CiaranMerritt <ciaran.merritt@colliewobble.com>
2023-03-20 20:33:18 +0000

    build: Adding the build into the project

--- vue-typescript/vite.config.ts
+++ vue-typescript/vite.config.ts
@@ -6,6 +6,7 @@ import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue_learning/vue-typescript/",
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
